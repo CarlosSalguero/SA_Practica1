7ad04c1477488d8c37d77021b4953b7609c92d7b
charlysa <carlos.bautista2008@gmail.com>
2021-06-17 00:18:36 -0600

    se agrega fabric3

---
+++ fabric/frontend.py
@@ -0,0 +1,18 @@
+from fabric import Connection
+
+from fabric import Connection
+
+c = Connection(
+    host="34.121.11.37",
+    user="testcollage619",
+    connect_kwargs={
+        "key_filename": "/key,
+        "passphrase": ""
+    },
+)
+
+with c.cd("practica2"):
+
+    result = c.run("whoami")
+    c.close()
+
