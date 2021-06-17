f4a7aea45b020e5d11af771257e1a6f1605fcf46
charlysa <carlos.bautista2008@gmail.com>
2021-06-16 22:18:15 -0600

    se agrega fabric3

--- fabric/backend.py
+++ fabric/backend.py
@@ -1,3 +1,3 @@
 from fabric import Connection
-connection = Connection(host="testcollage619@35.188.29.83", connect_kwargs = {"password":"JBFTAjm4cSwk"})
+connection = Connection(host="testcollage619@34.121.11.37", connect_kwargs = {"password":"JBFTAjm4cSwk"})
 connection.run("whoami")
